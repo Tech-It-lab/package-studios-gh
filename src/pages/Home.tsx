876dcc2783270b4177837f42cdee492b69b603bf
Carol King <carolking109@gmail.com>
2025-05-22 15:02:16 +0000

    showreel visible

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -78,7 +78,7 @@ const Home: React.FC = () => {
             loop
             playsInline
           >
-            <source src="/media/THIS IS GHANA Vol. 1 🇬🇭  (Drone Showreel).mp4" type="video/mp4" />
+            <source src="/media/This is ghana.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
